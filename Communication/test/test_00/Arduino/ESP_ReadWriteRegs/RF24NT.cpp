19f3d1456214ad27b3921a6f955c6d66fd650bd1
ECEQue <103890010+ECEQue@users.noreply.github.com>
2024-03-24 08:30:42 -0400

    Write strings to FRAM

--- Communication/test/test_00/Arduino/ESP_ReadWriteRegs/RF24NT.cpp
+++ Communication/test/test_00/Arduino/ESP_ReadWriteRegs/RF24NT.cpp
@@ -1,6 +1,66 @@
 #include "RF24NT.h"
 
-RF24NT::RF24NT(u_int8_t ce_pin, u_int8_t csn_pin):
+
+static const PROGMEM char rf24nt_datarate_e_str_0[] = "= 1 MBPS";
+static const PROGMEM char rf24nt_datarate_e_str_1[] = "= 2 MBPS";
+static const PROGMEM char rf24nt_datarate_e_str_2[] = "= 250 KBPS";
+
+static const PROGMEM char rf24nt_model_e_str_0[] = "nRF24L01";
+static const PROGMEM char rf24nt_model_e_str_1[] = "nRF24L01+";
+
+static const PROGMEM char rf24nt_crclength_e_str_0[] = "= Disabled";
+static const PROGMEM char rf24nt_crclength_e_str_1[] = "= 8 bits";
+static const PROGMEM char rf24nt_crclength_e_str_2[] = "= 16 bits";
+
+static const PROGMEM char rf24nt_pa_dbm_e_str_0[] = "= PA_MIN";
+static const PROGMEM char rf24nt_pa_dbm_e_str_1[] = "= PA_LOW";
+static const PROGMEM char rf24nt_pa_dbm_e_str_2[] = "= PA_HIGH";
+static const PROGMEM char rf24nt_pa_dbm_e_str_3[] = "= PA_MAX";
+static const PROGMEM char rf24nt_pa_dbm_e_str_4[] = "= PA_ERROR";
+
+static const PROGMEM char rf24nt_feature_e_str_on[]       = "= Enabled";
+static const PROGMEM char rf24nt_feature_e_str_allowed[]  = "= Allowed";
+static const PROGMEM char rf24nt_feature_e_str_open[]     = " open ";
+static const PROGMEM char rf24nt_feature_e_str_closed[]   = "closed";
+
+
+
+static const PROGMEM char* const rf24nt_datarate_e_str_P[] = {
+    rf24nt_datarate_e_str_0,
+    rf24nt_datarate_e_str_1,
+    rf24nt_datarate_e_str_2,
+};
+
+static const PROGMEM char* const rf24_model_e_str_P[] = {
+    rf24nt_model_e_str_0,
+    rf24nt_model_e_str_1,
+};
+
+static const PROGMEM char* const rf24_crclength_e_str_P[] = {
+    rf24nt_crclength_e_str_0,
+    rf24nt_crclength_e_str_1,
+    rf24nt_crclength_e_str_2,
+};
+
+static const PROGMEM char* const rf24_pa_dbm_e_str_P[] = {
+    rf24nt_pa_dbm_e_str_0,
+    rf24nt_pa_dbm_e_str_1,
+    rf24nt_pa_dbm_e_str_2,
+    rf24nt_pa_dbm_e_str_3,
+    rf24nt_pa_dbm_e_str_4,
+};
+
+
+static const PROGMEM char* const rf24_feature_e_str_P[] = {
+    rf24nt_crclength_e_str_0,
+    rf24nt_feature_e_str_on,
+    rf24nt_feature_e_str_allowed,
+    rf24nt_feature_e_str_closed,
+    rf24nt_feature_e_str_open,
+};
+
+
+RF24NT::RF24NT(uint8_t ce_pin, uint8_t csn_pin):
     RF24(ce_pin ,csn_pin, RF24NT_SPI_SPEED)
 {
     //Call the parent's constructor.
@@ -11,7 +71,7 @@ RF24NT::~RF24NT()
 }
 
 
-bool RF24NT::begin(u_int8_t rf_channel, rf24_pa_dbm_e rf_pa, rf24_datarate_e data_rate, rf24_crclength_e crc_length, bool auto_ack)
+bool RF24NT::begin(uint8_t rf_channel, rf24_pa_dbm_e rf_pa, rf24_datarate_e data_rate, rf24_crclength_e crc_length, bool auto_ack)
 {
 
     if(!(RF24::begin()))
@@ -28,20 +88,7 @@ bool RF24NT::begin(u_int8_t rf_channel, rf24_pa_dbm_e rf_pa, rf24_datarate_e dat
     return true;
 }
 
-inline char* RF24NT::getPALevel()
+char* RF24NT::getPALevel_str()
 {
-    uint8_t value = RF24::getPALevel();
-    switch (value)
-    { // MIN, LOW, HIGH, MAX, ERROR 
-    case 0:
-        return "MIN";
-    case 1:
-        return ("RF24_PA_LOW");
-    case 2:
-        return "HIGH";
-    case 3:
-        return "MAX";
-    default:
-        return "ERROR";
-    }
+    return (char*)(pgm_read_ptr(&rf24_pa_dbm_e_str_P[RF24::getPALevel()]));
 }
--- Communication/test/test_00/Arduino/ESP_ReadWriteRegs/RF24NT.h
+++ Communication/test/test_00/Arduino/ESP_ReadWriteRegs/RF24NT.h
@@ -1,8 +1,14 @@
 #ifndef RF24NT_h
 #define RF24NT_h
 
+#include <Arduino.h>
 #include <SPI.h>
+
+//#define MINIMAL     // Disable some functions and variables in RF24 lib that we need to redefine in our library. see line "#if !defined(MINIMAL)" in RF24.cpp 
 #include "RF24.h"
+
+
+
 
 #define RF24NT_SPI_SPEED 50000  //50 Mb
 
@@ -11,18 +17,17 @@ class RF24NT: public RF24
 private:
     /* data */
 public:
-    RF24NT(u_int8_t ce_pin, u_int8_t csn_pin);
+    RF24NT(uint8_t ce_pin, uint8_t csn_pin);
     ~RF24NT();
-
-    bool begin( 
-                u_int8_t rf_channel = 100, 
-                rf24_pa_dbm_e rf_pa = RF24_PA_MIN, 
-                rf24_datarate_e data_rate = RF24_250KBPS, 
-                rf24_crclength_e crc_length = RF24_CRC_DISABLED, 
-                bool auto_ack = false
-                        );
     
-    inline char* getPALevel();
+    char* getPALevel_str();
+    bool begin( 
+                uint8_t rf_channel         = 100, 
+                rf24_pa_dbm_e rf_pa         = RF24_PA_MIN, 
+                rf24_datarate_e data_rate   = RF24_250KBPS, 
+                rf24_crclength_e crc_length = RF24_CRC_DISABLED, 
+                bool auto_ack               = false
+                        );
 };
 
 
