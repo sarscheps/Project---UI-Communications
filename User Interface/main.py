f196b222a27227501581d8027dbc3f639c034041
Quotayba Yousif <quotayba.m@outlook.com>
2024-04-02 15:15:42 -0400

    ..

--- User Interface/main.py
+++ User Interface/main.py
@@ -9,12 +9,12 @@ from sidebar_ui import Ui_MainWindow
 
 class MainWindow(QMainWindow):
     def __init__(self):
-        super(MainWindow, self).__init()
+        super(MainWindow, self).__init__()
 
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-
+    '''
         self.ui.icon_only_widget.hide()
         self.ui.stackedWidget.setCurrentIndex(0)
         self.ui.home_btn_2.setChecked(True)
@@ -47,8 +47,7 @@ class MainWindow(QMainWindow):
 
     def on_dashboard_btn_2_toggled(self):
         self.ui.stackedWidget.setCurrentIndex(1)
-    
-
+    '''
 
 
 
@@ -58,9 +57,9 @@ if __name__ == "__main__":
 
 
     ## Loading style file
-    with open("style.qss", "r") as style_file:
-        style_str = style_file.read()
-    app.setStyleSheet(style_str)
+    #with open("style.qss", "r") as style_file:
+    #   style_str = style_file.read()
+    #app.setStyleSheet(style_str)
 
 
     window = MainWindow()
