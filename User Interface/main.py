2e6dae239f7edb206fdbebfa1910551b034df5b7
IrvingBecerril <irvingbecerrril@gmail.com>
2024-04-15 12:03:40 -0400

    main.py file edited to try and switch pages based on btns pressed

--- User Interface/main.py
+++ User Interface/main.py
@@ -1,9 +1,40 @@
-
-
 ## This is not working nor completed. 
 import sys
 from PyQt6.QtWidgets import QMainWindow, QApplication, QPushButton
-from MainWindow import MainWindow
+from WeatherApp_ui import Ui_MainWindow
+
+# Function for changing page to user page
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()    
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.ui.iconOnlyWidget.hide()
+        self.ui.pagesWidget.setCurrentIndex(0)
+        self.ui.homePageBtn.setChecked(True)
+
+        # Connect the buttons to their respective functions
+        self.ui.homePageBtn.clicked.connect(self.showHomePage)
+        self.ui.homePageIconBtn.clicked.connect(self.showHomePage)
+
+        self.ui.displayModeBtn.clicked.connect(self.showDisplayModePage)
+        self.ui.displayModeIconBtn.clicked.connect(self.showDisplayModePage)
+        
+        self.ui.syncWithDeviceBtn.clicked.connect(self.showSyncWithDevicePage)
+        self.ui.syncWithDeviceIconBtn.clicked.connect(self.showSyncWithDevicePage)
+        
+    def showHomePage(self):
+        self.ui.pagesWidget.setCurrentIndex(0)
+
+    def showDisplayModePage(self):
+        self.ui.pagesWidget.setCurrentIndex(1)
+
+    def showSyncWithDevicePage(self):
+        self.ui.pagesWidget.setCurrentIndex(2)
+
+
 
 
 if __name__ == "__main__":
@@ -18,6 +49,8 @@ if __name__ == "__main__":
 
     window = MainWindow()
     window.show()
+
+
 
 
     sys.exit(app.exec())
