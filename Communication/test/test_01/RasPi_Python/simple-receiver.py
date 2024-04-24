aff4bf1ffa37f65da1a1af85f7ddefd4330a1862
Quotayba Yousif <quotayba.m@outlook.com>
2024-04-24 11:02:39 -0400

    update the Communcation file for RasPi

--- Communication/test/test_01/RasPi_Python/simple-receiver.py
+++ Communication/test/test_01/RasPi_Python/simple-receiver.py
@@ -4,9 +4,76 @@ import struct
 import sys
 import time
 import traceback
+import os
+import csv
 
 import pigpio
 from nrf24 import *
+
+
+
+PRINT_DELAY = 1  # Second
+
+hostname    = "raspberrypi"
+address     = "1SNSR"
+spi_channel = "MAIN_CE0"
+port        = 8888
+spi_ce      = 25
+
+RF_Channel = 71
+RF_Pa      = RF24_PA.LOW
+data_rate  = RF24_DATA_RATE.RATE_1MBPS
+crc        = RF24_CRC.BYTES_2
+
+curr_dir = os.path.dirname(__file__)
+
+
+def hardwareTest() -> None:
+    
+        #Printing the Values used to initiate the nrf.
+        print("\n")
+        print("Start Hardware Test ...\n")
+        time.sleep(PRINT_DELAY)
+
+        print(f"Initial values:")                         
+        print(f"Connection channel: {RF_Channel}, PA level: {RF_Pa.name}")
+        print(f"Data rate: {data_rate.name}, CRC: {crc.name}")
+        print("\n")
+        
+        time.sleep(PRINT_DELAY)
+
+        #Start Assertion.
+        print("Start asserting ...\n")
+
+        #Asserting the RF Channel
+        print("Asserting the RF Channel ...")
+        time.sleep(PRINT_DELAY)
+        print(f"Channel = {nrf.get_channel()}")
+        assert nrf.get_channel() == RF_Channel
+        print("Done ...\n")
+
+        #Asserting the PA level
+        print("Asserting the PA level ...")
+        time.sleep(PRINT_DELAY)
+        print(f"PA level= {nrf.get_pa_level().name} ")
+        assert nrf.get_pa_level() == RF_Pa
+        print("Done ...\n")
+
+        #Asserting the data_rate
+        print("Asserting the data_rata ...")
+        time.sleep(PRINT_DELAY)
+        print(f"data_rate= {nrf.get_data_rate().name}")
+        assert nrf.get_data_rate() == data_rate
+        print("Done ...\n")
+
+        #Asserting the crc
+        print("Asserting the crc ...")
+        time.sleep(PRINT_DELAY)
+        print(f"CRC Bytes= {nrf.get_crc_bytes().name} ")
+        assert nrf.get_crc_bytes()  == crc
+        print("Done ...\n")
+
+        print("Test_00 is successfully done.")
 
 
 #
@@ -16,18 +83,6 @@ from nrf24 import *
 #
 if __name__ == "__main__":
 
-    print("Python NRF24 Simple Receiver Example.")
-    
-    # Parse command line argument.
-    parser = argparse.ArgumentParser(prog="simple-receiver.py", description="Simple NRF24 Receiver Example.")
-    parser.add_argument('-n', '--hostname', type=str, default='localhost', help="Hostname for the Raspberry running the pigpio daemon.")
-    parser.add_argument('-p', '--port', type=int, default=8888, help="Port number of the pigpio daemon.")
-    parser.add_argument('address', type=str, nargs='?', default='1SNSR', help="Address to listen to (3 to 5 ASCII characters)")
-
-    args = parser.parse_args()
-    hostname = args.hostname
-    port = args.port
-    address = args.address
 
     # Verify that address is between 3 and 5 characters.
     if not (2 < len(address) < 6):
@@ -42,48 +97,44 @@ if __name__ == "__main__":
         sys.exit()
 
     # Create NRF24 object.
-    # PLEASE NOTE: PA level is set to MIN, because test sender/receivers are often close to each other, and then MIN works better.
-    nrf = NRF24(pi, ce=25, payload_size=RF24_PAYLOAD.MAX, channel=100, data_rate=RF24_DATA_RATE.RATE_250KBPS, pa_level=RF24_PA.MIN)
+    # Using main spi with CE0.
+    # Connect RF_CSN pin to CE0(GPIO8) and connect the RF_CE to ce pin specified below (GPIO25).
+    nrf = NRF24(pi, ce=spi_ce, spi_channel=spi_channel, channel=RF_Channel ,spi_speed=50e3, pa_level=RF_Pa, data_rate=data_rate, crc_bytes=crc, payload_size=RF24_PAYLOAD.MAX,)
     nrf.set_address_bytes(len(address))
 
     # Listen on the address specified as parameter
     nrf.open_reading_pipe(RF24_RX_ADDR.P1, address)
     
     # Display the content of NRF24L01 device registers.
-    nrf.show_registers()
+    hardwareTest()
 
     # Enter a loop receiving data on the address specified.
     try:
         print(f'Receive from {address}')
-        count = 0
         while True:
 
             # As long as data is ready for processing, process it.
             while nrf.data_ready():
-                # Count message and record time of reception.            
-                count += 1
-                now = datetime.now()
+                current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
                 
                 # Read pipe and payload for message.
                 pipe = nrf.data_pipe()
-                payload = nrf.get_payload()    
+                payload:bytes[32] = nrf.get_payload()    
 
-                # Resolve protocol number.
-                protocol = payload[0] if len(payload) > 0 else -1            
+                print("Package Destination IP: {:08b}".format(payload[0]))
+                print("Sender IP: {:08b}".format(payload[1]))
+                print("Temp: {:.2f}".format(payload[2:5]))
+                print("Temp: {:.2f}".format(payload[6:9]))
 
-                hex = ':'.join(f'{i:02x}' for i in payload)
+                with open(os.path.join(curr_dir, "data_src/test_received_data.csv"),'w', newline='') as csvFile:
+                    writer = csv.writer(csvFile)
+                    # Write the data to the CSV file
+                    writer.writerow([current_time, "{:.2f}".format(payload[0]), "{:.2f}".format(payload[1])])
 
-                # Show message received as hex.
-                print(f"{now:%Y-%m-%d %H:%M:%S.%f}: pipe: {pipe}, len: {len(payload)}, bytes: {hex}, count: {count}")
 
-                # If the length of the message is 9 bytes and the first byte is 0x01, then we try to interpret the bytes
-                # sent as an example message holding a temperature and humidity sent from the "simple-sender.py" program.
-                if len(payload) == 9 and payload[0] == 0x01:
-                    values = struct.unpack("<Bff", payload)
-                    print(f'Protocol: {values[0]}, temperature: {values[1]}, humidity: {values[2]}')
-                
-                # Sleep 100 ms.
-                time.sleep(1)
+                csvFile.close()
+
+                time.sleep(0.25)
     except:
         traceback.print_exc()
         nrf.power_down()
